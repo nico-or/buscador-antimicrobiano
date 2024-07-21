e2f94b39341fe1d051febbef2c357325eab838d5
nicolas <nicordenes@gmail.com>
2024-07-21 17:03:52 -0400

    feat: first iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Medicamento } from "./types";
+import fuzzysort from "fuzzysort";
+import Card from "./components/card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState<Medicamento[]>([]);
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  const results = fuzzysort.go(query, data, {
+    threshold: 0.5,
+    all: false,
+    key: "nombre",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <label htmlFor="consulta" hidden={true}>
+          Consulta
+        </label>
+
+        <input
+          type="search"
+          name="consulta"
+          placeholder="Nombre del medicamento."
+          value={query}
+          onChange={handleChange}
+        />
+      </section>
+      <section>
+        {results.map((result, i) => {
+          return <Card key={i} medicamento={result.obj} />;
+        })}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Medicamento = {
+  nombre: string;
+  reconstitucion: string;
+  dilucion: string;
+  velocidad: string;
+  estabilidad: string;
+  ph: string;
+  osmolaridad: string;
+  riesgo: string;
+};
